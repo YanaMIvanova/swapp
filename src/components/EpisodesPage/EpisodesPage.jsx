f6cc80d49a9b94fd1bb6a99cb3cf286398951a03
Yana <yansy.blue.1998@gmail.com>
2019-10-16 01:49:29 +0300

    add basic episode card structure

--- src/components/EpisodesPage/EpisodesPage.jsx
+++ src/components/EpisodesPage/EpisodesPage.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import img from '../../assets/episode-cover.jpeg'
+import './EpisodesPage.scss'
 
 function EpisodesPage() {
-    return <div>EpisodesPage</div>
+    return (
+        <div className="episode-card">
+            <img src={img} alt="Episode Cover" className="episode-cover" />
+            <div className="episode-text">
+                <span className="episode-title">Episode Name</span>
+                <p>
+                    Lorem Ipsum is just a dummy text with a lot of unknown words. Here we go again! Lorem Ipsum is just
+                    a dummy text with a lot of unknown words. Here we go again
+                </p>
+            </div>
+        </div>
+    )
 }
 
 export default EpisodesPage
