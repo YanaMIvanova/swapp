14bffd880d5d818c0e4aafbc63863e68701e69cd
Yana <yansy.blue.1998@gmail.com>
2019-10-28 00:58:30 +0200

    add Login error text and validation

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,14 +1,31 @@
 import React, { useState } from 'react'
+import classnames from 'classnames'
 
 import styles from './LoginPage.module.scss'
 
 function LoginPage() {
     const [formState, setFormState] = useState({ username: '', password: '' })
+    const [invalidFields, setInvalidFields] = useState({ username: false, password: false })
+
+    const isInvalid = () => Object.values(invalidFields).includes(true)
+    const hasEmptyFields = () => Object.values(formState).includes('')
 
     const login = e => {
         e.preventDefault()
-        console.log(formState)
+
+        if (isInvalid() || hasEmptyFields()) {
+            return
+        }
+
         setFormState({ username: '', password: '' })
+    }
+
+    const validate = e => {
+        if (e.target.value.length <= 3) {
+            setInvalidFields({ ...invalidFields, [e.target.id]: true })
+        } else {
+            setInvalidFields({ ...invalidFields, [e.target.id]: false })
+        }
     }
 
     const change = e => {
@@ -20,25 +37,36 @@ function LoginPage() {
         <div className={styles.page}>
             <span className={styles.logo}>SWAPP</span>
             <form onSubmit={login} className={styles.form}>
+                {isInvalid() && <strong className={styles.errorText}>Invalid Credentials</strong>}
                 <input
+                    required
+                    onBlur={validate}
                     onChange={change}
                     value={formState.username}
-                    className={styles.input}
+                    className={classnames(styles.input, { [styles.invalid]: invalidFields.username === true })}
                     placeholder="Username"
                     type="text"
                     id="username"
                 />
                 <br />
                 <input
+                    required
+                    onBlur={validate}
                     onChange={change}
                     value={formState.password}
-                    className={styles.input}
+                    className={classnames(styles.input, { [styles.invalid]: invalidFields.password === true })}
                     placeholder="Password"
                     type="text"
                     id="password"
                 />
                 <br />
-                <input className={styles.submitButton} type="submit" value="Login" />
+                <input
+                    className={classnames(styles.submitButton, {
+                        [styles.disabled]: isInvalid() || hasEmptyFields(),
+                    })}
+                    type="submit"
+                    value="Login"
+                />
             </form>
         </div>
     )
